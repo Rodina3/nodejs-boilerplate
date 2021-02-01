36932ca0f9c2328544436487607a78d88d7552f8
Lixing <lixing.sun@thoughtworks.com>
2021-02-01 17:23:27 +0800

    add e2e tests for cat

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,38 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect({ status: 'available' });
   });
+
+  describe('CatController', () => {
+    it('GET /cats', () => {
+      return request(app.getHttpServer())
+        .get('/cats')
+        .expect(200)
+        .expect('This action retrieves all cats.');
+    });
+
+    it('GET /cats/:id', () => {
+      return request(app.getHttpServer())
+        .get('/cats/1')
+        .expect(200)
+        .expect('This action retrieve cat by id 1');
+    });
+
+    it('DELETE /cats/:id', () => {
+      return request(app.getHttpServer())
+        .delete('/cats/1')
+        .expect(501)
+        .expect({ message: 'Not implement yet' });
+    });
+
+    it('POST /cats', () => {
+      const validData = { name: 'Amy', age: 3, color: 'white' };
+      return request(app.getHttpServer())
+        .post('/cats')
+        .send(validData)
+        .expect(201)
+        .expect(
+          `This action adds cat with name: ${validData.name}, age: ${validData.age} and color: ${validData.color}`,
+        );
+    });
+  });
 });
